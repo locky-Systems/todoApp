475b8b76bd75ad03398abb5da587ba7630b703e7
luck <loicksauvage49@gmail.com>
2024-10-02 15:47:55 +0200

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Tessa Ichambe on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
